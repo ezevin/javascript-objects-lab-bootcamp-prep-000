710bf614a4713c88be6ac9c5e5f8702801dc36c9
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-15 16:08:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  object[prop] =1, object[prop2] = 2
 }
 function deleteFromObjectByKey(object, key) {
-  Object.assign({prop:1}, {prop2:2})
+ 
   var obj = Object.assign({}, object)
+  return object:key
   delete obj[key]
 }
 function destructivelyDeleteFromObjectByKey(object, key) {Object.assign({prop:1}, {prop2:2})
