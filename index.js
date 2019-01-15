439cf8b69ad777d6508ff3c9ffeb397a7d215d9b
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-15 15:26:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
  return Object.assign({prop:1}, {prop2:2})
 }
-
-
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
  object[prop] =1, object[prop2] = 2
 }
 function deleteFromObjectByKey(object, key) {
-  
-  var obj = {prop:'1'}; {prop2:'2'}
+  var obj = {prop:'1'}
+  var obj2 = {prop2:'2'}
   var recipes = Object.assign({}, obj)
-  recipes
   delete recipes.prop
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
