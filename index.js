35d0acc8a2903424f45def9ad592e941e4eaad69
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-15 14:29:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var recipes = {}
+
 function objects() {recipes = {prop: 1} }
-function updateObjectWithKeyAndValue(object, key, value) {recipes.prop = 1; recipes.prop2 = 2
-  var obj = {prop:1}
+
+function updateObjectWithKeyAndValue(object, key, value)
+
 }
 
 
