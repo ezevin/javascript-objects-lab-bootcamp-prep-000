8df4e9dbacc4f16cf7cf59139fbbefcb62e45b61
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-15 15:13:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  const object =prop:1; prop2:2
- 
 }
 function deleteFromObjectByKey(object, key) {
   
