5cd8fd589b12f90e1d08416a569626a1a1fdd8ce
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-15 15:35:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign ({}, 'prop')
   delete newObj.prop
 }
-function destructivelyDeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {Object.assign({prop:1}, {prop2:2})
+delete object.prop
   
 }
