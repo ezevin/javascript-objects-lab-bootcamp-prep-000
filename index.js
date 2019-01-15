a206355f868e9fc4dfb69ecace06dac8b349db71
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-15 16:25:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function deleteFromObjectByKey(object, key) {
   delete obj[key]
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
-  delete object.key;
+  var obj = {prop:1}
+  delete obj.prop;
 }
