ef6bbf9e2504b0193018b45869ddb8d62fce9d83
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-15 14:25:53 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {recipes.prop = 1; recipes.prop2 = 2
+  var obj = {prop:1}
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ 
+}
+function deleteFromObjectByKey(object, key) {
+  
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
